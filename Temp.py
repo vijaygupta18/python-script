fa2ce1e3e6757b46f5c15387306a3f8bbb3934e4
Akhilesh Singh Bhadauriya <akhileshsingh2314@gmail.com>
2024-02-06 12:39:23 +0530

    added few more thing
    asdklfasdklf

---
+++ Temp.py
@@ -0,0 +1,3 @@
+import random
+for i in range(1000):
+    print("\"shard-"+str(random.randint(0,127))+"\",", end=" ")
--- test.py
+++ test.py
@@ -1,36 +1,12 @@
-import requests
+pathOfFile = "/home/akhilesh/Desktop/projects/nammayatri/Backend/dynamic-offer-driver-app-exe.log"
+print(pathOfFile)
+def processFile():
+    # Read the file and process it
+    with open(pathOfFile, 'r') as f:
+        for line in f:
+            # Do something with the line
+            if "Latency for redisFindAll" in line:
+                value = line.find("Latency for redisFindAll")
+                print(line[value:-3])
 
-# Define your URLs
-FIRST_URL = "https://api.sandbox.beckn.juspay.in/context/list"
-SECOND_URL = "https://api.sandbox.beckn.juspay.in/context"
-
-# Define your headers
-headers = {
-    'Authorization': 'Bearer 12345678',
-    'X-Tenant': 'atlas_driver_offer_bpp_v2'
-}
-
-def get_context_ids(url):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return [context["id"] for context in data]
-    else:
-        print(f"Failed to fetch data: {response.status_code}")
-        return []
-
-def delete_context(id):
-    response = requests.delete(f"{SECOND_URL}/{id}", headers=headers)
-    if response.status_code == 200:
-        print(f"Successfully deleted context with id: {id}")
-    else:
-        print(f"Failed to delete context with id: {id}, Status Code: {response.status_code}")
-
-def main():
-    context_ids = get_context_ids(FIRST_URL)
-    print(context_ids)
-    for context_id in context_ids:
-        delete_context(context_id)
-
-if __name__ == "__main__":
-    main()
+processFile()
