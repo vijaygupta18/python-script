7f4e85a305ecc83bd77176c423ed5e9fd0b0c10e
ratnadeep.b <ratnadeep.b@juspay.in>
2024-02-29 01:25:58 +0530

    CAC: Fixed various bugs in createDefaultConfig.py (Maybe testing still pending).

--- CAC/createDefuaultConfig.py
+++ CAC/createDefuaultConfig.py
@@ -19,9 +19,9 @@ password = ''
 
 # Other Misc vars
 schema_name = '' # Let this be empty
-table_names = ["merchant_service_usage_config"]
+table_names = ["transporter_config"]
 env = 'local'
-app = 'v2'
+app = 'dobpp'
 cac_tgt_url = 'http://localhost:8080'
 tenant = 'test'
 where_column = 'merchant_operating_city_id'
@@ -54,7 +54,7 @@ def main(table_name):
   if app == 'dobpp':
     if env == 'master': merchantOpCityId = '1e7b7ab9-3b9b-4d3e-a47c-11e7d2a9ff98'
     elif env == 'prod': merchantOpCityId = 'f067bccf-5b34-fb51-a5a3-9d6fa6baac26'
-    else: merchantOpCityId = 'a174b699-a3f3-b362-22a1-602951dbe8a2' # For local replace this !!!!!!!!!
+    else: merchantOpCityId = 'ea1a6af7-f9d3-3f3e-3542-e5dc196dd44a' # For local replace this !!!!!!!!!
     schema_name = 'atlas_driver_offer_bpp'
   else:
     if env == 'master': merchantOpCityId = 'b30daaf7-77d2-17c8-00d9-baf7ad0f5719'
@@ -85,7 +85,8 @@ def main(table_name):
       # Fetch the results
       results = cursor.fetchall()
 
-      column_names = convertToCamelCase([desc[0] for desc in cursor.description])
+      snk_cols = [desc[0] for desc in cursor.description] # col names in snake case
+      column_names = convertToCamelCase(snk_cols)
       print("col names :", column_names)
 
       # Process the results
@@ -103,21 +104,22 @@ def main(table_name):
         url = f'{cac_tgt_url}/default-config/{new_table_name}:{column_names[j]}'
         print ("result is : ", results[0][j])
         try:
-          if (results[0][j] == None): #TODO: Test This Module.
-            cursor.execute(f"SELECT data_type FROM information_schema.columns WHERE table_name = {table_name} AND column_name = {column_names[j]}")
-            type = cursor.fetchone()[0]
-            if type.lower() == 'numeric' or type.lower() == 'integer' or type.lower() == 'bigint':
-               type = 'Number'
-            elif type.lower() == 'decimal' or type.lower() == 'real':
-               type = 'Decimal'
+          if (results[0][j] == None or results[0][j] == "None"): #TODO: Test This Module.
+            cursor.execute(f"SELECT data_type FROM information_schema.columns WHERE table_name LIKE '{table_name}' AND column_name = '{snk_cols[j]}'")
+            typ = cursor.fetchone()[0]
+            print("The Type Of Maybe Field is : ", typ)
+            if typ.lower() == 'numeric' or typ.lower() == 'integer' or typ.lower() == 'bigint':
+               typ = 'Number'
+            elif typ.lower() == 'decimal' or typ.lower() == 'real':
+               typ = 'Decimal'
             else :
-               type = 'String'
-            if type != 'String':
-              data = {"value":None,"schema":{"type":"Maybe" + " " + type}}
+               typ = 'String'
+            if typ != 'String':
+              data = {"value":"None","schema":{"type":"Maybe" + " " + typ}}
             else:
                print(f"WARNING:- Could not decode type of the maybe column {column_names[j]} hence putting as null as string type. Check This !!!!!!! ")
-               data = {"value":"null","schema":{"type":"String"}}
-          if(type(results[0][j]) == int):
+               data = {"value":"null","schema":{"type":"string","pattern":".*"}}
+          elif (type(results[0][j]) == int):
             data = {"value":int(results[0][j]),"schema":{"type":"number"}}
           elif (type(results[0][j]) == bool):
             data = {"value":bool(results[0][j]),"schema":{"type":"boolean"}}
@@ -127,13 +129,15 @@ def main(table_name):
             if(":" in str(results[0][j])) and ("{" in str(results[0][j]) and "[" not in results[0][j]):
               print("adding this value", results[0][j])
               data = {"value":(str(results[0][j])),"schema":{"type":"string","pattern":".*"}}
-            elif type(results[0][j]) == list:
-              print("adding this value (Array of int)", results[0][j])
-              data = {"value":(results[0][j]),"schema":{"type":"array"}}
+              #UNCOMMENT THE BELOW CODE ONCE COMPATIBILITY FOR ARRAYS IS ADDED.
+            # elif type(results[0][j]) == list:
+            #   print("adding this value (Array of int)", results[0][j])
+            #   data = {"value":(results[0][j]),"schema":{"type":"array"}}
             else:
               data = {"value":rm_sq(str(results[0][j])),"schema":{"type":"string","pattern":".*"}}
-        except:
-            print("Skipping column since cannot parse :-\ :", column_names[j])
+        except Exception as e:
+            print(f"WARNING: Skipping column since cannot parse :-\ : {column_names[j]} with error : {e} !!!!!!!!!!")
+            continue
 
         # print("Url is :", url)
         # print("Data is :", data)
